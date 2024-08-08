aed99065a455381126f50035b6cc3ecaf3dd8f26
VadimKrut <vadim.zaletaev@mail.ru>
2024-08-08 21:07:22 +0300

    sadfas

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     const greetingElement = document.getElementById("greeting");
-    greetingElement.textContent = "Привет хуй";
+    greetingElement.textContent = "Привет хуй1";
 });
